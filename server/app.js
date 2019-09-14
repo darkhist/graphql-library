953a6e3f90732b030d25c599c24cf4cd87fcd590
darkhist <qmsalas@iastate.edu>
2019-09-13 22:47:13 -0500

    add cors

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const db = require('./db');
 
 const app = express();
+app.use(cors());
 
 app.use('/api', graphqlHTTP({
   schema: schema,
