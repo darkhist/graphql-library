df47f7840e7ecfc706d3825b0b80ca32577e4f80
darkhist <qmsalas@iastate.edu>
2019-09-13 22:47:00 -0500

    scaffold React frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import BookList from './components/BookList'
+
+const App = () => {
+  return (
+    <div className="main">
+      <h1> GraphQL Library </h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
