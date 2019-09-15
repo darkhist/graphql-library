63262c9b2e70a105f58e67b1ab36c1a1c44563a4
darkhist <qmsalas@iastate.edu>
2019-09-15 00:43:07 -0500

    supply new schema and resolver objects to graphql-express to serve the API

--- server/app.js
+++ server/app.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema');
+const { schema, resolvers } = require('./schema/schema');
 const db = require('./db');
 
 const app = express();
 app.use(cors());
 
-app.use('/api', graphqlHTTP({
-  schema: schema,
-  // use the graphiql IDE
-  graphiql: true
-}));
+app.use(
+  '/api',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true
+  })
+);
 
 app.listen(9000, () => {
   console.log('Listening on port 9000');
